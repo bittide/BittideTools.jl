6a7c85eca900d7632a52fe19c7c078ab19501314
Sanjay Lall <sanjaylall@google.com>
2025-08-29 15:00:47 -0700

    Add link status support for Elara

--- src/framing.jl
+++ src/framing.jl
@@ -1,14 +1,13 @@
 module Framing
 
-export write_popped_data_to_processor_memory, step_processor_one_cycle, read_data_from_processor_memory
+export read_data_from_processor_memory, set_link_status, step_processor_one_cycle,
+       write_popped_data_to_processor_memory
 
 # Functions called by the Frames module of Callisto
 # and implemented by Elara.
 write_popped_data_to_processor_memory(fdp::Nothing, i, port, localtick, data) = return
 step_processor_one_cycle(fdp::Nothing, i, localtick) = return
 read_data_from_processor_memory(fdp::Nothing, i, port, localtick)::UInt64 = 0x0
-
-
+set_link_status(fdp::Nothing, i, port, status::Int64) = return
 
 end
-
